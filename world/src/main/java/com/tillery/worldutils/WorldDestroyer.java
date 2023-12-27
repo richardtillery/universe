43806f3e02ac4ffe508cf7ee7776b39730d73d18
Richard Tillery <richard.tillery@gmail.com>
2023-12-26 20:44:02 -0500

    work in progress

---
+++ world/src/main/java/com/tillery/worldutils/WorldDestroyer.java
@@ -0,0 +1,5 @@
+package com.tillery.worldutils;
+
+public class WorldDestroyer {
+    //TODO: make some methods to help destroy worlds
+}
--- world/src/main/module-info.java
+++ world/src/main/module-info.java
@@ -1,3 +1,5 @@
 module world {
-    exports com.tillery.world
+    exports com.tillery.world; //export package
+    // can't export a package that isn't real
+    //exports com.tillery.notreal;
 }
